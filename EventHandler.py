9e901d71c9a40e495e6a212e0020e41f238828d2
noella <noelladsa@gmail.com>
2015-03-30 18:31:54 -0400

    Class handles events from the 2048 view

---
+++ EventHandler.py
@@ -0,0 +1,18 @@
+#Handles all e
+class EventHandler(object):
+
+	"""Class handles all events from the view"""
+	def __init__(self, gridsize = 4):
+		super(EventHandler, self).__init__()
+		self.gridsize = gridsize
+		self.data = 
+
+
+	def HandleKeyDown(self):
+
+
+	def HandleKeyUp(self):
+
+	def HandleKeyRight(self):
+
+	def HandleKeyLeft(self):
