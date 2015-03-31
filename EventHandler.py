536eaf22edc39e0e97030741e4f750de0ad7d8c8
noella <noelladsa@gmail.com>
2015-03-31 17:09:17 -0400

    Added Generate tile function

--- EventHandler.py
+++ EventHandler.py
@@ -1,18 +1,32 @@
-#Handles all e
+#!/usr/bin/python
+
+import random
+
 class EventHandler(object):
+	"""Handles all key event operations coming in from the 2048 view"""
 
-	"""Class handles all events from the view"""
-	def __init__(self, gridsize = 4):
+	def __init__(self, gridsize = 4):	
 		super(EventHandler, self).__init__()
+		""" Starts of a with a default board"""
 		self.gridsize = gridsize
-		self.data = 
 
+	def StartGame():
+		self.data = [[]]
 
-	def HandleKeyDown(self):
+	def GenerateRandomTile(self):
+		coord = {}
+		coord["tileX"] = random.randint(0,self.gridsize) #Assuming X start from the top left and increases as you go right
+		coord["tileY"] = random.randint(0,self.gridsize) #Assuming Y start from the top left and increases as you go to the bottom
+		return coord
 
+	def LoadBoard(self):
+		pass
 
-	def HandleKeyUp(self):
+	def PrintBoard(self):
+		for x in range
 
-	def HandleKeyRight(self):
+	def Moveleft(self):
+		pass
 
-	def HandleKeyLeft(self):
+	def MoveRight(self):
+		pass
