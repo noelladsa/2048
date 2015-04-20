16b90b5e299fa35cee6a425d2be544be52264e4f
noella <noelladsa@gmail.com>
2015-04-20 13:42:03 -0400

    Model.py

--- Model.py
+++ Model.py
@@ -2,122 +2,130 @@
 import random
 import pickle
 import os.path
-import copy
 import numpy as np
-from ListOperations import ListOperations
+import ListOps
+
 
 class Model(object):
-	"""Handles all key event operations coming in from the 2048 view"""
+    """Handles all key event operations coming in from the 2048 view"""
+    FILE_NAME = "2048.p"
 
-	def __init__(self, size = 4):	
-		""" Starts of a with a default board"""
-		self.size = size									#Size of the edge of a grid
-		self.grid = np.zeros((size,size),dtype=np.int)		#Using numpy for easier array translation
-		self.max_score = 0
-		# if os.path.isfile("2048.p"):
-		# 	self.max_score = pickle.load( open( "2048.p", "rb" ) )
-		self.listeners = []
+    def __init__(self, size=4):
+        self.size = size
+        # Using numpy for easy array transforms
+        self.grid = np.zeros((size, size), dtype=np.int)
+        self._load_score()
+        self.listeners = []
 
-	def start_game(self):
-		self.score = 0
-		self._add_number()
+    def _load_score(self):
+        self.max_score = 0
+        if os.path.isfile(self.FILE_NAME):
+            self.max_score = pickle.load(open(self.FILE_NAME, "rb"))
 
-	def register_listener(self,listener):
-		self.listeners.append(listener)
+    def _save_score(self):
+        pickle.dump(self.max_score,open(self.FILE_NAME, "wb"))
 
-	def remove_listener(self,listener):
-		self.listeners.remove(listener)
+    def start_game(self):
+        self.score = 0
+        self._add_number()
 
-	def send_notification():
-		pass
+    def register_listener(self, listener):
+        self.listeners.append(listener)
 
-	def get_score(self):									 #Getters in python?
-		return self.score
-		
-	def _add_number(self): 
-		"""Find an empty spot and add new tile"""
-		occupied_tiles = set()
-		while len(occupied_tiles) <= self.size * self.size:
-			coord = (random.randint(0,self.size - 1),random.randint(0,self.size - 1)) 
-			if coord in occupied_tiles:
-				continue
-			elif self.grid[coord[0]][coord[1]] == 0:
-				self.grid[coord[0]][coord[1]] = random.choice([2,4])
-				for listener in self.listeners: 			#Send notifications
-					listener.number_added(coord[0],coord[1],self.grid[coord[0]][coord[1]])
-				return True
-			else:
-				occupied_tiles.add(coord)
+    def remove_listener(self, listener):
+        self.listeners.remove(listener)
 
-		return False	
+    def send_notification(self, notification):
+        for listener in self.listeners:
+            listener.notify(notification)
 
-	def print_board(self):
-		""" Printing exising board contents """
-		print "Printing board"
-		for x in range(0, self.size):
-			for y in range(0, self.size):
-				if (self.grid[x][y] == 0):
-					print "-",
-				else:
-				 	print self.grid[x][y],
-			print "\r"
-	
-	def move_left(self): 
-		""" Moving numbers to the left extreme of the board """
-		ops_log = []
-		for y in range(0,self.size): #Iterating from top to bottom
-			ops_log_row = ListOperations.collapse_list(self.grid[y,:])
-			ops_log = ops_log + ops_log_row
+    def get_score(self):
+        return self.score
 
-		if len(ops_log) > 0:
-			self._add_number()
+    def get_max_score(self):
+        return self.max_score
 
+    def _get_rand_coord(self):
+        """ Returns a random value coord"""
+        return random.randint(0, self.size-1), random.randint(0, self.size-1)
 
-	def move_right(self): 
-		""" Moving numbers to the right extreme of the board """
-		ops_log = []
-		for y in range(0,self.size): #Iterating from top to bottom
-			ops_log_row = ListOperations.collapse_list(self.grid[y,::-1])
-			ops_log = ops_log + ops_log_row
+    def _add_number(self):
+        """Find an empty spot and add new tile"""
+        occupied_tiles = set()
+        while len(occupied_tiles) <= self.size * self.size:
+            x, y = self._get_rand_coord()
+            if (x, y) in occupied_tiles:                        # Already checked this spot in a prev iteration
+                continue
+            elif self.grid[x][y] == 0:
+                self.grid[x][y] = random.choice([2, 4])
+                self.send_notification({'State': ListOps.NEW, 'to_x': x,
+                                        "to_y": y, "val": self.grid[x][y]})
+                return True
+            else:
+                occupied_tiles.add((x, y))
+        return False
 
-		if len(ops_log) > 0:
-			self._add_number()
-	
-	def move_up(self): 
-		""" Moving numbers to the top extreme of the board """
-		ops_log = []
-		for x in range(0,self.size): #Iterating from left to right
-			ops_log_row = ListOperations.collapse_list(self.grid[:,x])
-			ops_log = ops_log + ops_log_row
+    def get_board_string(self):
+        """ String rep of the board"""
+        string = ""
+        for row in self.grid:
+            for element in row:
+               eleStr= " - " if element == 0 else " "+str(element)+" "
+               string = string +eleStr
+            string = string + "\n"
+        return string
 
-		if len(ops_log) > 0:
-			self._add_number()
-	
-	def move_down(self): 
-		""" Moving numbers to the bottom extreme of the board """
-		ops_log = []
-		for x in range(0,self.size): #Iterating from left to right
-			ops_log_row = ListOperations.collapse_list(self.grid[::-1,x])
-			ops_log = ops_log + ops_log_row
+    def _process_results(self, logs, axis):
+        if all(not log for log in logs):
+            return
 
-		if len(ops_log) > 0:
-			self._add_number()
+        self._add_number()
+        for log in logs:
+            for entry in log:
+                if entry["State"] == ListOps.ADD:
+                    self.score = self.score + entry["to_val"]
+        return axis, logs
 
-	def _has_empty_spaces(self):
-		for x in range(0,self.size):
-			for y in range(0,self.size):
-				if self.grid[x][y] == 0 : return True
-		return False
+    def move_left(self):
+        """ Moving numbers to the left extreme of the board """
+        return self._process_results([ListOps.collapse_list(self.grid[y, :], 0)
+                                      for y in range(self.size)], "y")
 
-	def is_game_over(self):
-		""" Checking if number pairs are available in a full grid/ If further moves are possible"""
-		if self._has_empty_spaces() == True:
-			return False
+    def move_right(self):
+        """ Moving numbers to the right extreme of the board """
+        return self._process_results([ListOps.collapse_list(self.grid[y, ::-1], self.size)
+                                      for y in range(self.size)], "y")
 
-		for y in range(0,self.size):
-			if(ListOperations.is_pair_present(self.grid[y,:]) is True):
-				return False
-		for x in range(0,self.size):
-			if(ListOperations.is_pair_present(self.grid[:,x]) is True):	
-				return False
-		return True
+    def move_up(self):
+        """ Moving numbers to the top extreme of the board """
+        return self._process_results([ListOps.collapse_list(self.grid[:, x], 0)
+                                      for x in range(self.size)], "x")
+
+    def move_down(self):
+        """ Moving numbers to the bottom extreme of the board """
+        return self._process_results([ListOps.collapse_list(self.grid[::-1, x], self.size)
+                                      for x in range(self.size)], "x")
+
+    def _has_empty_spaces(self):
+        for x in range(0, self.size):
+            for y in range(0, self.size):
+                if self.grid[x][y] == 0:
+                    return True
+        return False
+
+    def is_game_over(self):
+        """ Checking if number pairs are available in a full grid/ If further moves are possible"""
+        if self._has_empty_spaces():
+            return False
+
+        for y in range(0, self.size):
+            if(ListOps.is_pair_present(self.grid[y, :])):
+                return False
+        for x in range(0, self.size):
+            if(ListOps.is_pair_present(self.grid[:, x])):
+                return False
+        if self.score > self.max_score:
+            self.max_score = self.score
+            self._save_score()
+
+        return True
