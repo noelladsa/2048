e21f5ab563fabd4bcbe4505fc42c112625211c01
noella <noelladsa@gmail.com>
2015-05-22 22:34:22 -0400

    Removed print statement

--- Controller.py
+++ Controller.py
@@ -5,30 +5,30 @@ from View import View
 
 class Controller(object):
 
-	def __init__(self):
-		self.handler = Model()
-		self.view = View(self)
-		self.handler.register_listener(self.view)
+    def __init__(self):
+        self.handler = Model()
+        self.view = View()
+        self.view.set_controller(self)
+        self.handler.register_listener(self.view)
 
-	def start(self):
-		print "Start"
-		self.handler.start_game()
-		self.view.load_view()
-	
-	def handle_key_left(self):
-		self.handler.move_left()
+    def start(self):
+        self.handler.start_game()
+        self.view.load_view()
 
-	def handle_key_right(self):
-		self.handler.move_right()
+    def handle_key_left(self):
+        self.handler.move_left()
 
-	def handle_key_up(self):
-		self.handler.move_up()
-	
-	def handle_key_down(self):
-		self.handler.move_down()
+    def handle_key_right(self):
+        self.handler.move_right()
 
-	def handle_quit():
-		pass
+    def handle_key_up(self):
+        self.handler.move_up()
+
+    def handle_key_down(self):
+        self.handler.move_down()
+
+    def handle_quit():
+        pass
 
 
 controller = Controller()
